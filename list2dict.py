883378e4d899a30bebe77f03abd119fceba4d244
Matt Hannan <matthannan1@gmail.com>
2017-08-10 15:57:08 -0400

    Further with adding dictionaries to a dictionary

--- list2dict.py
+++ list2dict.py
@@ -7,25 +7,60 @@ import os
 ## create a root Tk widget (so we can destroy it later)
 root = Tkinter.Tk()
 
-# GUI file picker
-filePath = tkFileDialog.askopenfilename(initialdir = "C:\Users\hannamj\Dropbox\Public\genealogy\$FamilyTree_GED\Gephi",
-                                            title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+print "GUI file picker"
 
-# Create empty nodes list
-nodes = []
+nodePath = tkFileDialog.askopenfilename(initialdir = 
+                                        "C:\Users\hannamj\Dropbox\Public\genealogy\$FamilyTree_GED\Gephi",
+                                        title = "Select Match file",
+                                        filetypes = (("csv files","*.csv"),("all files","*.*")))
 
-# Open the file
-with open(filePath, 'rb') as infile:
-    data = csv.reader(infile)
+edgePath = tkFileDialog.askopenfilename(initialdir = 
+                                        "C:\Users\hannamj\Dropbox\Public\genealogy\$FamilyTree_GED\Gephi",
+                                        title = "Select ICW file",
+                                        filetypes = (("csv files","*.csv"),("all files","*.*")))
 
-    # Grab first row as headers    
-    fields = data.next()
+print "Create empty lists"
+nodeData = []
+edgeData = []
+nodeDict = {}
 
-    # Grab each additional row
-    for row in data:
-        # Zip together the field names and values
-        item = dict(zip(fields, row))   
+print "Open the files"
+with open(edgePath, 'rb') as edgeFile:
+    edge = csv.reader(edgeFile)
+    edgeData = list(edge)
 
-    for k, v in item.items():
-        print(k,v)
+with open(nodePath, 'rb') as nodeFile:
+    node = csv.reader(nodeFile)
+    
+
+    print "Pump file contents into nodes list"
+    for row in node:
+        nodeData.append(row)
+        nodeID = row[11]
+        nodeDictEntry = {}
         
+        print "Read the column names from the first line of the file"
+        nodeFields = nodeData[0]
+        print "Pop off first row (the headers)"
+        nodeData.pop(0)
+        print "Now we have Headers and nodes objects"
+
+        print "Grab each additional row"
+        for nodeRow in nodeData:
+            print "Zip together the field names and values to create Dictionary nodeDictEntry"
+            nodeDictEntry.update(dict(zip(nodeFields, nodeRow)))
+
+            print "Build ICW list"
+            icwList = []
+            for edgeRow in edgeData:
+                if edgeRow[5] == nodeRow[11]:
+                    icwList.append(edgeRow[6])
+
+            print "Add icwList to Dictionary nodeDictEntry"
+            nodeDictEntry.update({'ICW':icwList})
+            print nodeDictEntry
+            input()
+        # Add Dictionary nodeDictEntry to Dictionary nodeDict
+        nodeDict[nodeID] = (nodeDictEntry)
+print nodeDict
+print len(nodeDict) 
