afedde859652677cea121dd1bbe41a5d2787842e
Matt Hannan <matthannan1@gmail.com>
2017-08-17 15:18:25 -0400

    dictTestNest.py - fixed up strange printing issue on exit

--- dictTestNest.py
+++ dictTestNest.py
@@ -6,21 +6,24 @@ run = True
 
 while run:
     userInput = raw_input("Press 1 for ages, 2 for eyes, 3 for all, or 4 to end: ")
-    print
-    for k, v in mainDict.items():
-        print k
-        details = mainDict[k]
-        if userInput == '1':
-            print "Age:  ", details.get('age')
 
-        if userInput == '2':
-            print "Eyes: ", details.get('eyes')
+    if userInput == '4':
+        run = False
 
-        if userInput == '3':
-            print "Age:  ", details.get('age')
-            print "Eyes: ", details.get('eyes')
-
-        if userInput == '4':
-            run = False
-
+    else:
         print
+
+        for k, v in mainDict.items():
+            print k
+            details = mainDict[k]
+            if userInput == '1':
+                print "Age:  ", details.get('age')
+
+            if userInput == '2':
+                print "Eyes: ", details.get('eyes')
+
+            if userInput == '3':
+                print "Age:  ", details.get('age')
+                print "Eyes: ", details.get('eyes')
+
+            print
